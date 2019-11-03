51e783a2aa2561c28eb24a4b462c1848b02888b6
Mvalad01 <mvalad01@gmail.com>
2019-11-02 21:17:12 -0600

    changed sounds

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -9,70 +10,106 @@ public class Rocket : MonoBehaviour
     AudioSource audioSource;
     [SerializeField] float rcsThrust = 200f;
     [SerializeField] float thrustPower = 20f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deadSound;
+    [SerializeField] AudioClip winSound;
+    enum State { Alive, Dying, Trasending }
+    State state = State.Alive;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
     }
-
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
     }
-
     private void ProcessInput()
     {
-        Thrust();
-        Control();
+        if (state == State.Alive)
+        {
+            RespondToThrust();
+            RespondToControl();
+        }
     }
-
-    private void Control()
+    private void RespondToControl()
     {
-        
+
         rigidbody.freezeRotation = true;
-        float rotationThisFrame = rcsThrust*Time.deltaTime;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
-        {   
+        {
             transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward * rotationThisFrame); 
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
         rigidbody.freezeRotation = false;
     }
-
-    private void Thrust()
+    private void RespondToThrust()
     {
-        
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up*thrustPower);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
         }
         else
         {
-            audioSource.Stop();
+            if (state != State.Trasending)
+                audioSource.Stop();
         }
     }
 
-    void OnCollisionEnter(Collision collision){
+    private void ApplyThrust()
+    {
+        rigidbody.AddRelativeForce(Vector3.up * thrustPower);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) return;
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-            
-            break;
+                break;
             case "Finish":
-            print("WIN!!");
-            break;
+                FinishSequence();
+                break;
             default:
-            print("ded");
-            break;
+                FailSequence();
+                break;
         }
+    }
+
+    private void FailSequence()
+    {
+        audioSource.Stop();
+        audioSource.PlayOneShot(deadSound);
+        state = State.Dying;
+        Invoke("LoadFirstScene", 1f);
+    }
+
+    private void FinishSequence()
+    {
+
+        audioSource.PlayOneShot(winSound, 1);
+        state = State.Trasending;
+        Invoke("LoadNextScene", 1f);//TODO sterializw
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
